295139fc2bdef8b6786b5530f18b06806e227a78
Andres Paredes <andresparedes202@gmail.com>
2024-03-23 18:48:00 -0400

    start

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,23 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        themegreen: "#008000",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
 };
 export default config;
